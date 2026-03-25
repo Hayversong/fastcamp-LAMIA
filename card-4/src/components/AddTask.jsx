2c9aa918f13938ad7d11bc39745b005f7e57f345
Hayverson <hayversongab@outlook.com>
2026-03-25 13:31:15 -0300

    feat: area para adicionar tarefas

--- card-4/src/components/AddTask.jsx
+++ card-4/src/components/AddTask.jsx
@@ -1,6 +1,10 @@
 function AddTask(){
-    return (
-        <h1>Add Task</h1>
+    return(
+        <div className=" space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+            <input type="text" placeholder="Título da tarefa" className="border border-slate-400 outline-slate-400 px-4 py-2 rounded-md" />
+            <input type="text" placeholder="Descrição da tarefa" className="border border-slate-400 outline-slate-400 px-4 py-2 rounded-md" />
+            <button>adicionar</button>
+        </div>
     )
 }
 
