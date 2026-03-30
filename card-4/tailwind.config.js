ddac357437f7083b6473031753cb3f02a5c40db3
Hayverson <hayversongab@outlook.com>
2026-03-30 17:26:00 -0300

    fix: correcao tela de tarefa

--- card-4/tailwind.config.js
+++ card-4/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js, ts, jsx, tsx}", "./**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // Removida a linha "./**/*.{js,ts,jsx,tsx}" que causava o loop no node_modules
+  ],
   theme: {
     extend: {},
-  },  
+  },
   plugins: [],
 };
