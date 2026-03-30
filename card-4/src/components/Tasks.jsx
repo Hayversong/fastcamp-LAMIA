e5df3dc239811bff8e6eee7c0e33fbebc5e2fe91
Hayverson <hayversongab@outlook.com>
2026-03-30 18:33:56 -0300

    feat: inclusao de nova arte nas tarefas

--- card-4/src/components/Tasks.jsx
+++ card-4/src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -19,8 +19,9 @@ function Tasks(props) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => props.onTaskClick(task.id)} className={
-            `bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted && 'line-through'}`
+            `bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md ${task.isCompleted && 'line-through'}`
             }>
+            {task.isCompleted && <CheckIcon />}
                 {task.title}
           </button>
           <Button onClick={() => onSeeDetails(task)} >
