5f9399080233f63a43a69f868dfb0cfe391b121c
Hayverson <hayversongab@outlook.com>
2026-08-07 15:54:28 -0300

    feat: funções de deletar e editar

--- card-8/codigo_aula/fast_zero/src/fast_zero/app.py
+++ card-8/codigo_aula/fast_zero/src/fast_zero/app.py
@@ -1,8 +1,8 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from fast_zero.schema import Message, UserSchema, UserPublic, UserDB
+from fast_zero.schema import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
@@ -13,11 +13,39 @@ database = []
 def read_root():
     return {'message': 'Olá Mundo!'}
 
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
 
-    user_with_id = UserDB(
-        id=len(database) + 1, **user.model_dump()
-    )
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+
+    database.append(user_with_id)
 
     return user_with_id
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(status_code=404, detail='NOT FOUND')
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(status_code=404, detail='NOT FOUND')
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
